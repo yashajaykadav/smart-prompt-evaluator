89dbe953c6a6defa70a98dd3867406c0371b8e7a
YASHKADAV <110305731+yashajaykadav@users.noreply.github.com>
2026-04-09 12:43:37 +0530

    Delete check_model.py

--- check_model.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyAkmyZBVsyGX2EsSgmnJQ8Krjrm7zPSXJI")
-print("Checking available models for your key...")
-
-for model in client.models.list():
-    print(f"-> {model.name}")
